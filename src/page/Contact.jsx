c42119b6b8695de0874ca9d6532d5ce5de32ccd7
LauraKalmanova <bubamarley7@gmail.com>
2023-12-11 11:03:38 +0100

    Add contact form, collect values and console log them

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -2,10 +2,30 @@ import Footer from "../partial/Footer";
 import Header from "../partial/Header";
 
 const Contact = () => {
+
+  const handleContactForm = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+    console.log(email, message);
+  }
+
   return (
     <>
       <Header />
-      <p>Page contact</p>
+      <form onSubmit={handleContactForm}>
+        <label>
+          Email 
+          <input type="email" name="email"></input>
+        </label>
+        <label>
+          Message 
+          <input type="text" name="message"></input>
+        </label>
+        <label> 
+          <input type="submit"></input>
+        </label>
+      </form>
       <Footer />
     </>
   );
